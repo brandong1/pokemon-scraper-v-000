353e91605b4d3adf856703aac112d03c75893acb
Brandon Green <brandon@brandonleeproductions.com>
2018-08-09 22:57:43 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,17 @@
 class Pokemon
+  attr_accessor :name, :type, :db
+
+  def initialize(name, type, db)
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+end
+
+def self.all? 
+  @@all
+end
+
+def self.save
+
 end
